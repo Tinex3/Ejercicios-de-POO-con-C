6ccb47c83d9678883e153165f9ac8d0aaccabb83
Tinex3 <b.riquelme.gomez@gmail.com>
2024-01-13 18:30:01 -0300

    Update Registro de Contactos.c

--- Registro de Contactos.c
+++ Registro de Contactos.c
@@ -1,6 +1,7 @@
 /*Registro de Contactos: Diseña un programa que gestione un
 registro de contactos. Cada contacto debe tener información
-como nombre, número de teléfono y dirección. Implementa funciones para añadir, mostrar y buscar contactos.*/
+como nombre, número de teléfono y dirección. Implementa funciones para añadir,
+mostrar y buscar contactos.*/
 #include <stdio.h>
 #include <string.h>
 
@@ -20,7 +21,7 @@ void InicializarContato(struct Contacto *contacto, const char *Nombre, const cha
 
 void AgregarContato(struct Contacto *contacto)
 {
-    
+
     printf("Por favor, Ingrese el nombre de su contacto: \n");
     scanf("%s", contacto->Nombre);
 
@@ -28,22 +29,43 @@ void AgregarContato(struct Contacto *contacto)
     scanf("%s,", contacto->Direccion);
 
     printf("Por favor, Ingresar el numero de su contacto");
-    scanf("%d",contacto->NumeroTelefonico);
+    scanf("%d", contacto->NumeroTelefonico);
 
-    while(getchar() != '\n');
+    while (getchar() != '\n')
+        ;
 }
 
-void mostrarcontacto(struct Contacto *contacto){
+void mostrarcontacto(struct Contacto *contacto)
+{
     printf("El nombre de tu contacto es: %s\n", contacto->Nombre);
     printf("La direccion de tu contacto es: %s\n", contacto->Direccion);
     printf("El numero de tu contacto es: %d\n", contacto->NumeroTelefonico);
 }
 
+// void imprimircontacto(struct Contacto *contactos in){
+//     for(int i = 0; i < cantidad; i++){
+//         printf("Nombre del contacto %d: %s\n" i + 1,);
+//     }
+// }
 int main()
 {
-
+    int SelectorDeEstado;
     while (1)
     {
+        printf("Selecciones 1 si quiere buscar un contacto\n");
+        printf("Selecciones 2 si quiere agregar un nuevo contacto\n");
+        scanf("%s", SelectorDeEstado);
+        switch (SelectorDeEstado)
+        {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        default:
+            break;
+        }
     }
     return 0;
 }
